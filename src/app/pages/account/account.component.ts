48b8c8c3d8c4802cf5ce11f87033e0eaa0c0f9ad
VivianDomingues <viviandomingues04@gmail.com>
2022-08-08 20:30:38 -0300

    Aula rotas

---
+++ src/app/pages/account/account.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent implements OnInit {
+
+  public name: string | undefined;
+  private route: ActivatedRoute;
+
+  constructor(route: ActivatedRoute) { 
+    this.route = route;
+  }
+
+  //Observer (Objeto consumidor), next, error, complete
+  ngOnInit(): void {
+    this.route.params.subscribe({
+      next: (params) => {
+        this.name = params["name"];
+      } ,
+      error: (error) => { //caso tenha um erro
+        console.log(error);
+      },
+      complete: () => { //completar ciclo de consumo de dados
+        console.log("Completo!");
+      }
+      });
+  }
+
+}
