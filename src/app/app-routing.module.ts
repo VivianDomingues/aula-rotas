96a0ec1bdbff42fb08f582e2da4ce98f0beb18cb
VivianDomingues <viviandomingues04@gmail.com>
2022-08-09 16:43:32 -0300

    Guards (canActive, canDeactive, canActivateChild)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExitGuard } from './guards/exit.guard';
+import { LoginSubrotasGuard } from './guards/login-subrotas.guard';
+import { LoginGuard } from './guards/login.guard';
 import { AccountComponent } from './pages/account/account.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -16,7 +19,8 @@ const routes: Routes = [
   },
   {
     path:"home",
-    component: HomeComponent
+    component: HomeComponent,
+    canActivate: [LoginGuard]
   },
   {
     path:"login",
@@ -24,6 +28,8 @@ const routes: Routes = [
   },
   {
     path:"dashboard",
+    canActivateChild: [LoginSubrotasGuard],
+    canDeactivate: [ExitGuard],
     children: [ // rotas filhas
       {
         path: "",
--- src/app/guards/exit.guard.ts
+++ src/app/guards/exit.guard.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExitGuard implements CanDeactivate<unknown> {
+  //Permitir/Bloquear a saida da rota.
   canDeactivate(
-    component: unknown,
+    component: DashboardComponent,
     currentRoute: ActivatedRouteSnapshot,
     currentState: RouterStateSnapshot,
     nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return window.confirm("Deseja sair da pagina?");
   }
   
 }
